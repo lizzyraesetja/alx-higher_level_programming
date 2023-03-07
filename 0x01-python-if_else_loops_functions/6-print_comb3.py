46847f1e26bec8405a1800489eb5db03d74f340c
lizzyraesetja <lizzraesetja2@gmail.com>
2023-03-07 03:10:08 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
-    print("{:02d}".format(i), end=", ")
-print("{:02d}".format(99))
+for i in range(0, 8):
+    for j in range(i, 10):
+        if i != j:
+            print("{:d}{:d}, ".format(i, j), end='')
+print("89")
