c4241cc2f53443a009c3a8ed35ff8b8cc7b1fbeb
lizzyraesetja <lizzraesetja2@gmail.com>
2023-03-07 05:58:16 -0800

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,12 +1,68 @@
- #include <stdio.h>
+#include "lists.h"
 
-int main(void) {
-    int number;
+listint_t *create_node(int n);
 
-    printf("Enter a number: ");
-    scanf("%d", &number);
+/**
+ * insert_node - inserts a node sorted in a linked list of ints
+ * @head: double pointer to head of LL, needed for modification in edge
+ * cases
+ * @number: data for new node
+ *
+ * Return: pointer to newly created node, NULL on failure
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *cur_node = NULL, *new_node = NULL;
 
-    printf("The number you entered is %d\n", number);
+	if (!head)
+		return (NULL);
+	else if (!(*head))
+	{
+		new_node = create_node(number);
+		*head = new_node;
+		return (new_node);
+	}
+	cur_node = *head;
+	while (cur_node)
+	{
+		/* need to insert at head */
+		if (cur_node->n >= number)
+		{
+			new_node = create_node(number);
+			new_node->next = cur_node;
+			*head = new_node;
+			return (new_node);
+		}
+		else if (cur_node->n <= number)
+		{
+			if (!cur_node->next || cur_node->next->n >= number)
+			{
+				new_node = create_node(number);
+				new_node->next = cur_node->next;
+				cur_node->next = new_node;
+				return (cur_node->next);
+			}
+		}
+		cur_node = cur_node->next;
+	}
+	return (NULL); /* failed */
+}
 
-    return 0;
+
+/**
+ * create_node - creates a new node for the LL
+ * @n: data to insert into new node
+ *
+ * Return: pointer to newly allocated node
+ */
+listint_t *create_node(int n)
+{
+	listint_t *ret = NULL;
+
+	ret = malloc(sizeof(listint_t));
+	if (!ret)
+		return (NULL);
+	ret->next = NULL;
+	ret->n = n;
+	return (ret);
 }
