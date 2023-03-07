946cc4b3e2910ac93b1fb9ea4fbb00f28f5bacca
lizzyraesetja <lizzraesetja2@gmail.com>
2023-03-07 08:30:47 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-for i in range(1, 10):
+for i in range(9):
     for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print(i, j)
-        else:
-            print(i, j, end=', ')
+        if i * 10 + j < 89:
+            print("{:d}{:d}".format(i, j), end=", ")
+print("{:d}".format(89))
