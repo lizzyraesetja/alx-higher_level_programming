3dfb59486895df01121de961f3433a69e9ef7819
lizzyraesetja <lizzyraesetja2@gmail.com>
2023-03-13 12:34:24 +0000

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,26 +1,10 @@
 #!/usr/bin/python3
 
-dd_tuple(tuple_a=(), tuple_b=()):
-    """
-    adds the first two elements of two tuples together
-    and returns the result
-    """
-    ta_len = len(tuple_a)
-    tb_len = len(tuple_b)
-    new_tup = ()
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    r = []
+    a = iter(tuple_a)
+    b = iter(tuple_b)
     for i in range(2):
-        if i >= ta_len:
-            val_a = 0
-        else:
-            val_a = tuple_a[i]
-        if i >= tb_len:
-            val_b = 0
-        else:
-            val_b = tuple_b[i]
-
-        if (i == 0):
-            new_tup = (val_a + val_b)
-        else:
-            new_tup = (new_tup, val_a + val_b)
-
-    return (new_tup)
+        r.append(next(a, 0) + next(b, 0))
+    return tuple(r)
