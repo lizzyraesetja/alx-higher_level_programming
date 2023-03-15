e4389f7665a8c25db6bc44f6dec6d2ae3205a116
lizzyraesetja <lizzyraesetja2@gmail.com>
2023-03-15 20:46:29 +0000

    3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    # Creating an empty list.
+    # Empty curly braces {} will make an empty dictionary in Python. To make a
+    # set without any elements we use the set() function without any argument.
+    #
+    # a = {}
+    # a = set()
+    #
+    return(set_1 & set_2)
