539131f21b14ebaea5779805bb427962d102dc9e
lizzyraesetja <lizzraesetja2@gmail.com>
2023-04-04 00:52:31 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,16 +6,15 @@ This module provides a simple Rectangle class with attribute width and
 Default values of both attributes are 0."""
 
 class Rectangle:
-    
     """A Rectanagle class with attributes width and height"""
     def _int_(self,width=0,height=0):
         self.width = width
         self.height = height
-        
+
         @property
         def width(self):
             return selft._width
-        
+
         @width.setter
         def width(self,values):
             if type(value) is not int:
@@ -23,11 +22,11 @@ class Rectangle:
             if value < 0:
                 raise ValueError('Width must >= 0')
             self._width = value
-            
+
             @property
             def height(self):
                 return self._height
-            
+
             @height.setter
             def height(self,value):
                 if type(value) is not int:
