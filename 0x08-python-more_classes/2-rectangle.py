6fddf48bb539c42a27145b834c6c9f6a5a1f3c94
lizzyraesetja <lizzraesetja2@gmail.com>
2023-04-05 00:39:23 -0700

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,43 +1,61 @@
 #!/usr/bin/python3
-"""This is the "Rectangle" module.
+"""This module  has a class for rectangule instances.
+The module has the class Rectangle.
+"""
 
-This module provides a simple Rectangle class."""
 
 class Rectangle:
-    """A Rectangle class with attributes width and height, and methods area and perimiter."""
+    """class rectangle.
+    the class defines the height and width prived
+    instance attributes, getter and setter method for each.
+    area and permiter methods
+    """
 
-    def _int_(self, width=0, height=0):
-        self.width = width
-        self.height = height
+    def __init__(self, width=0, height=0):
+        if (type(height) != int):
+            raise TypeError("height must be an integer")
+        elif (height < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+        if (type(width) != int):
+            raise TypeError("width must be an integer")
+        elif (width < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
 
-        @property
-        def width(self):
-            return self._width
+    def area(self):
+        return self.height * self.width
 
-        @width.setter
-        def.width(self, value):
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value <0:
-                raise ValueError("width must be >= 0")
-            self._width = value
+    def perimeter(self):
+        if (self.height == 0 or self.width == 0):
+            return 0
+        else:
+            return 2 * self.height + 2 * self.width
 
-        @property
-        def height(self):
-            return self._height
+    @property
+    def height(self):
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value < 0
-            raise ValueError("Height must be >0")
-        self._height = value
+    @height.setter
+    def height(self, value):
+        if (type(value) != int):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
-        def area(self):
-            return self._width * self._height
+    @property
+    def width(self):
+        return self.__width
 
-        def perimiter(self):
-            if self._width is not 0 or self._height is 0:
-                return 0
-            return (2* self.width) + (2 * selft.height)
+    @width.setter
+    def width(self, value):
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
