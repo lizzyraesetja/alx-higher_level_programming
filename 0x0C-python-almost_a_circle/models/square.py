a09039763a96740d98119f21b3a4fddb8c5aba70
lizzyraesetja <lizzyraesetja2@gmail.com>
2023-04-25 10:27:39 +0000

    square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,74 +1,55 @@
 #!/usr/bin/python3
 """
-    contains class Square implements class Rectangle
+This module implements a square object
 """
-from models.rectangle
-import Rectangle
+from models.rectangle import Rectangle
+
 
 class Square(Rectangle):
-    """
-        Square implements rectangle
-    """
-    def __init__(self, size, x=0, y=0, id=None):
-        """
-        initialises Square (overrides Rectangle init)
+    """Square implementstion"""
+
+    def __init__(self, size: int, x=0, y=0, id=None):
+        """initialization
         """
         super().__init__(size, size, x, y, id)
+        self.__size = size
 
     @property
-    def size(self):
+    def size(self) -> int:
+        """size getter
         """
-        returns the size of the square
-        """
-        return self.width
+        return self.__size
+
     @size.setter
-    def size(self, value):
+    def size(self, value: int):
+        """size setter
         """
-        sets the value of size
-        """
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        self.__size = value
+        self.width = self.height = value
 
-        self.width = value
-        self.height = value
+    def __str__(self) -> str:
+        """string representation"""
+        id = self.id
+        size = self.__size
+        x = self.x
+        y = self.y
+        return "[Square] ({}) {}/{} - {}".format(id, x, y, size)
 
-        def update(self, *args, **kwargs):
-        """
-            assigns key/value argument to attributes
-            kwargs is skipped if args is not empty
-            Args:
-               *args -  variable number of no-keyword args
-                **kwargs - variable number of keyworded args
-        """
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__setattr__(key, val)
-            return
+    def update(self, *args, **kwargs):
+        """update arguments"""
+        attr = ['id', 'size', 'x', 'y']
+        if args:
+            for at, numb in zip(attr, args):
+                setattr(self, at, numb)
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key in attr:
+                    setattr(self, key, value)
 
-        try
-            self.id = args[0]
-            self.size = args[1]
-            self.x = args[2]
-            self.y = args[3]
-        except IndexError:
-            pass
-
-    def _str_(self):
-        """
-            Overloading str function
-        """
-        return "[{}] ({}) {}/{} - {}".format(type(self).__name__,
-                                                           self.id, self.x, self.y,
-                                             self.width)
-
-        def to_dictionary(self):
-            """
-            Returns the dictionary representation of a Square
-        """
-
-        return {'id': getattr(self, "id"),
-                'size': getattr(self, "width"),
-                'x': getattr(self, "x"),
-                'y': getattr(self, "y")}
+    def to_dictionary(self) -> dict:
+        """squaere to dictionary"""
+        id = self.id
+        size = self.__size
+        x = self.x
+        y = self.y
+        return {'id': id, 'x': x, 'size': size, 'y': y}
