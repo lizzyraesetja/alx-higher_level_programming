83c6d0e594eb391c9c6d6c1149b38ee2d134b942
lizzyraesetja <lizzyraesetja2@gmail.com>
2023-06-23 09:26:50 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Safe from SQL injections.
-# Usage: ./3-my_safe_filter_states.py <mysql username> \
-#                                     <mysql password> \
-#                                     <database name> \
-#                                     <state name searched>
-import sys
+"""Filter states by user input"""
 import MySQLdb
 
+
+def main():
+
+    if len(argv) == 5:
+        # connect
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=argv[1],
+                             passwd=argv[2],
+                             db=argv[3])
+        # cursor
+        c = db.cursor()
+
+        # execute query
+        c.execute("SELECT * FROM states WHERE name = %s\
+        ORDER BY states.id ASC", (argv[4], ))
+
+        # fetch
+        rows = c.fetchall()
+
+        # print
+        for row in rows:
+            print(row)
+
+        # close
+        c.close()
+        db.close()
+    else:
+        return
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    from sys import argv
+    main()
