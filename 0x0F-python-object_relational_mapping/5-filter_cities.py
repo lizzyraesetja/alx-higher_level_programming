99952ed05f169eb99b5b74933a3a8af3bba73ae2
lizzyraesetja <lizzyraesetja2@gmail.com>
2023-06-23 09:33:33 -0700

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state name searched>
-import sys
-import MySQLdb
+'''script to list all cities with user inputted state'''
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    from sys import argv
+    import MySQLdb
+
+    # connect
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3]
+                         )
+    # cursor
     c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    # execute
+    state = (argv[4], )
+    cmd = "SELECT cities.name FROM cities JOIN states ON\
+    cities.state_id = states.id AND states.name = %s ORDER BY cities.id ASC"
+
+    c.execute(cmd, state)
+
+    # fetch
+    rows = c.fetchall()
+
+    # print
+    ll = [x[0] for x in rows]
+    together = ", ".join(ll)
+    print(together)
+
+    # close
+    c.close()
+    db.close()
