f464ba6050983f84fe4d736f71cf6721f484206a
lizzyraesetja <lizzyraesetja2@gmail.com>
2023-06-23 09:44:09 -0700

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
-# Prints the first State object from the database hbtn_0e_6_usa.
-# Usage: ./8-model_state_fetch_first.py <mysql username> /
-#                                       <mysql password> /
-#                                       <database name>
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+# print first state objects from the database
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy.engine.url import URL
 
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
-        print("Nothing")
-    else:
-        print("{}: {}".format(state.id, state.name))
+    # url
+    url = {'drivername': 'mysql+mysqldb',
+           'host': 'localhost',
+           'port': '3306',
+           'username': argv[1],
+           'password': argv[2],
+           'database': argv[3]}
+
+    c_url = URL(**url)
+
+    # create engine, metadata for stored objects
+    engine = create_engine(c_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # session
+    session = Session(engine)
+
+    try:
+        first = session.query(State).first()
+        print("{}: {}".format(first.id, first.name))
+    except:
+        print('Nothing')
+
+    # close
+    session.close()
